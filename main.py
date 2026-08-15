c80fcb9a24fae2a7e4f38635c0c1867910749ce7
BGRAMPAGE <saigamer224@gmail.com>
2026-08-15 10:49:55 +0530

    Use uvicorn logger

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 from pydantic import BaseModel
 
+
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("uvicorn.error")
 
 import joblib
 
